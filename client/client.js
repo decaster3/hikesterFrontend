8e6d78a24079718e19381e682db296c338ac68a7
Rinat Hatipov <rinathatipov@MacBook-Air-Rinat.local>
2017-04-10 00:51:45 +0300

    first commit

---
+++ client/client.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+class Main extends Component {
+    render(){
+        return (
+          <p>Lets1133 ewfeqweqwqewfgo!</p>
+        );
+    }
+}
+render (<Main />, window.document.getElementById('app'))
